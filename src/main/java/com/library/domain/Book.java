ba1e87e0f567cf0dd5c91383d3144d2146f9f194
Kiankk <kiandehghani2@gmail.com>
2026-05-02 16:07:42 -0400

    Add Book class extending Item

---
+++ src/main/java/com/library/domain/Book.java
@@ -0,0 +1,46 @@
+package com.library.domain;
+
+/**
+ * A book copy in the library catalog.
+ */
+public class Book extends Item {
+
+    private String isbn;
+    private String author;
+    private String genre;
+
+    /**
+     * @param id     unique copy id
+     * @param title  book title
+     * @param status initial lifecycle status
+     * @param isbn   13-digit ISBN
+     * @param author author name
+     * @param genre  genre label
+     */
+    public Book(String id, String title, ItemStatus status, String isbn, String author, String genre) {
+        super(id, title, status);
+        this.isbn = isbn;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    /** @return the 13-digit ISBN */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /** @return the author name */
+    public String getAuthor() {
+        return author;
+    }
+
+    /** @return the genre label */
+    public String getGenre() {
+        return genre;
+    }
+
+    @Override
+    public String describe() {
+        return null;
+    }
+}
